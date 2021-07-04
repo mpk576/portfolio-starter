40dd64bbc7c3a795da88866a5405a43a5eeac43f
Mark Keenan <{ID}+{username}@users.noreply.github.com>
2021-07-04 19:52:13 +0100

    - Remove node_modules and .next folder from repo.

---
+++ .next/static/webpack/webpack.7963c472b8a992a78c76.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "de09657ebe9b5e39a4b1"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay43OTYzYzQ3MmI4YTk5MmE3OGM3Ni5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcImRlMDk2NTdlYmU5YjVlMzlhNGIxXCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
